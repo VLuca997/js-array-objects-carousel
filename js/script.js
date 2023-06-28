1b1272881b3be1427725d3410a82a875db1ced5b
VLuca997 <kulertoast997@gmail.com>
2023-06-28 18:11:33 +0200

    REMOVE AND APPLICATION NEW CODE .

--- js/script.js
+++ js/script.js
@@ -7,35 +7,65 @@
 
 //Array Carosello immagini
 const imagesCarousel = [
-    "img/01.webp",
-    "img/02.webp",
-    "img/03.webp",
-    "img/04.webp",
-    "img/05.webp"
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
 ];
 
-//seleziono gli elementi html del carosello e dei bottoni
-let carouselDiv = document.querySelector('.carousel');
-let arrowUp = document.querySelector('.arrow-up');
-let arrowDown = document.querySelector('.arrow-down');
-let imgIndex = 0;// impostiamo il corrente indice dell'immagine a 0 per renderla attiva.
+const itemContainer = document.querySelector('.items');
 
+imagesCarousel.forEach((singleImagesObject, i, imagesArray) => {
 
-// Ciclo for per generare il markup delle immagini nel carosello
-for (let i = 0; i < imagesCarousel.length; i++){
-    let img = document.createElement('img'); // creo un elemento img
-    img.classList.add('carousel-img'); //gli attribuiscoi la classe carousel-img
-
-    if (i === 0){ // Se l'indice è 0
-        img.classList.add('active'); // Aggiungiamo anche la classe active per rendere l'elemento visibile
+    let itemClasses = 'item';
+    if (i === 0){
+        itemClasses += ` active`
     }
-    img.src = imagesCarousel[i]; // dico all'elemento src che l'indice di questa immagine corrisponde all'elemento creato.
-    carouselDiv.appendChild(img); // aggiungo questo elemento come figlio di carousel.img
-}
 
-// Creo gli event listner per i button
-arrowUp.addEventListener('click', scrollUp);
-arrowDown.addEventListener('click', scrollDown);
+
+    itemContainer.innerHTML += `
+        <div class="${itemClasses}">
+            <img src="${singleImagesObject.image}" alt="${singleImagesObject.title}">
+            <div class="texts">
+                <h3>
+                ${singleImagesObject.title}
+                </h3>
+                <p>
+                    ${singleImagesObject.text}
+                </p>
+            </div> 
+        </div>
+    
+    `
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -70,3 +100,6 @@ function scrollDown(){
     newActive.classList.add('active');
 
 }
+// Creo gli event listner per i button
+arrowUp.addEventListener('click', scrollUp);
+arrowDown.addEventListener('click', scrollDown);
